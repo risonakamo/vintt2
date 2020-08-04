8946f2aaca83dcdf5e3f035101de942b8d2aff09
risonakamo <hmykanae@gmail.com>
2020-08-03 23:17:06 -0400

    comments remove config folder

--- @types/types.d.ts
+++ @types/types.d.ts
@@ -1,15 +1,17 @@
+// main program configuration json schema
 interface VinttConfiguration
 {
     [exeName:string]:string
+}
+
+// time.json schema
+interface ProgramTimes
+{
+    [programName:string]:number
 }
 
 interface FoundProgramResult
 {
     exe:string
     name:string
-}
-
-interface ProgramTimes
-{
-    [programName:string]:number
 }
