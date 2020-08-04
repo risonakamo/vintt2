b1de77920137fba5ec816d831647344c3048ebaf
risonakamo <hmykanae@gmail.com>
2020-08-03 22:31:37 -0400

    initial setup of timing function

--- vintt2.ts
+++ vintt2.ts
@@ -1,10 +1,14 @@
 import fs from "fs-extra";
 import psList from "ps-list";
 import _ from "lodash";
+import NanoTimer from "nanotimer";
+import log from "log-update";
+import stripIndent from "strip-indent";
 
 async function main()
 {
-    console.log(await watchPrograms(getConfiguration()));
+    var foundProgram:FoundProgramResult=await watchPrograms(getConfiguration());
+    timeProgram(foundProgram);
 }
 
 // retrieve vintt configuration
@@ -26,6 +30,7 @@ function getConfiguration():VinttConfiguration
 async function watchPrograms(config:VinttConfiguration):Promise<FoundProgramResult>
 {
     return new Promise((resolve)=>{
+        log("watching...");
         var exeNames:Set<string>=new Set(Object.keys(config));
         var interval=setInterval(async ()=>{
             var foundProgram:ProcessDescriptor|undefined=_.find(await psList(),
@@ -44,6 +49,24 @@ async function watchPrograms(config:VinttConfiguration):Promise<FoundProgramResu
             }
         },2000);
     });
+}
+
+// begin timing a program. displays on screen various timing information and about
+// the currently running program. writes to file on the program.
+function timeProgram(program:FoundProgramResult):void
+{
+    var timer:NanoTimer=new NanoTimer();
+    var currentTime:number=0;
+
+    timer.setInterval(()=>{
+        log(stripIndent(`
+            ${program.name}
+            Current Session: ${currentTime} minutes
+            Total Time: 2.1 hours
+        `));
+
+        currentTime+=60;
+    },"","1.5s");
 }
 
 main();
